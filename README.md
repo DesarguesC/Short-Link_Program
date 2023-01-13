bd17e56959a121c5e6c480aac77434e8420a9809
stein_wang <stein_wang@qq.com>
2023-01-13 22:17:17 +0800

    fix create

--- README.md
+++ README.md
@@ -3,3 +3,33 @@
 origin short-link-fronted: https://github.com/MellyWangBeauty/short_link_frontend
 
 origin short-link-backend: https://github.com/DesarguesC/short-link-backend
+
+配置文件模板 conf.yaml
+
+\# conf.yaml
+
+sql:
+
+ username: root # mysql 用户名
+
+ password:  **     # mysql 密码
+
+ db_name: website_test
+
+
+
+account: # 初始帐户
+
+ admin_user: admin
+
+ admin_passwd: passwd
+
+
+
+server:
+
+ secret: secret # jwt 密钥
+
+
+
+debug: true # 开启 CORS
--- short-link-backend/model/url.dto.go
+++ short-link-backend/model/url.dto.go
@@ -6,15 +6,15 @@ type CreateInput struct { // 前端输入
 	Origin     string    `gorm:"type:varchar(200)" form:"origin" json:"origin"`
 	Short      string    `gorm:"type:varchar(200)" form:"short" json:"short"`
 	Comment    string    `gorm:"type:varchar(100)" form:"comment" json:"comment"`
-	StartTime  time.Time `gorm:"type:datetime;autoCreateTime" json:"start-time"`
-	ExpireTime time.Time `gorm:"type:datetime" json:"expire-time"`
+	StartTime  time.Time `gorm:"type:datetime;autoCreateTime" json:"startTime"`
+	ExpireTime time.Time `gorm:"type:datetime" json:"expireTime"`
 }
 
 type UpdateInput struct {
 	Origin     string    `gorm:"type:varchar(200)"  json:"origin"`
 	Comment    string    `gorm:"type:varchar(100)"  json:"comment"`
-	StartTime  time.Time `gorm:"type:datetime;autoCreateTime" json:"start-time"`
-	ExpireTime time.Time `gorm:"type:datetime" json:"expire-time"`
+	StartTime  time.Time `gorm:"type:datetime;autoCreateTime" json:"startTime"`
+	ExpireTime time.Time `gorm:"type:datetime" json:"expireTime"`
 }
 
 type DelInput struct {
@@ -32,5 +32,5 @@ type ProfileInput struct {
 }
 type ProfileOutput struct {
 	Time       time.Time `gorm:"type:datetime;autoCreateTime" json:"time"`
-	AccessTime time.Time `gorm:"type:datetime;autoCreateTime" json:"access-time"`
+	AccessTime time.Time `gorm:"type:datetime;autoCreateTime" json:"accessTime"`
 }
